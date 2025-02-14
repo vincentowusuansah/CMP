0b5dc3f8bb86a7d8f53e0d5f53bebbfc399e482c
vincentowusuansah <kojo1006789@gmail.com>
2025-02-14 19:58:25 +0100

    Second comiit

---
+++ scripts/projects.js
@@ -0,0 +1,47 @@
+let projects = [
+  {
+    name: "project one",
+    tags: "#javascript, #ui/ux, #backend",
+    image: "project1.jpg",
+  },
+  {
+    name: "project two",
+    tags: "#javascript, #fullstack, #ui/ux, #backend",
+    image: "project2.jpg",
+  },
+  {
+    name: "project three",
+    tags: "#javascript, #ui/ux, #backend",
+    image: "project3.jpg",
+  },
+  {
+    name: "project four",
+    tags: "#javascript, #fullstack",
+    image: "project4.jpg",
+  },
+  {
+    name: "project five",
+    tags: "#javascript, #fullstack, #ui/ux, #backend",
+    image: "project5.jpg",
+  },
+  {
+    name: "project six",
+    tags: "#javascript, #fullstack",
+    image: "project6.jpg",
+  },
+  {
+    name: "project seven",
+    tags: "#javascript, #ui/ux, #backend",
+    image: "project7.jpg",
+  },
+  {
+    name: "project eight",
+    tags: "#javascript, #fullstack",
+    image: "project8.jpg",
+  },
+  {
+    name: "project nine",
+    tags: "#javascript, #ui/ux, #backend",
+    image: "project9.jpg",
+  },
+];
